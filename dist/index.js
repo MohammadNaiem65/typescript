d776b25b2a1296db826330dbcba1e356b3300abd
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-05-01 21:12:02 +0600

    add: explored type alias

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+// Creating same type of objects normally
+const person1 = {
+    name: 'John',
+    age: 30,
+};
+const person2 = {
+    name: 'John',
+    age: 30,
+};
+const person3 = {
+    name: 'John',
+    age: 30,
+};
+const person4 = {
+    name: 'John',
+    age: 30,
+};
+const add = (a, b) => a + b;
+console.log(add(1, 2));
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,42 @@
-// Object
-const user: {
-    firstName: string;
-    middleName?: string; // Optional property (type)
-    lastName: string;
-    readonly isEmployed: true; // Literal type - read-only property
-} = {
-    firstName: 'Rifat',
-    lastName: 'Tusar',
-    isEmployed: true,
+// Creating same type of objects normally
+
+const person1: { name: string; age: number } = {
+    name: 'John',
+    age: 30,
 };
+
+const person2: { name: string; age: number } = {
+    name: 'John',
+    age: 30,
+};
+
+// Creating same type of objects using type alias
+type Person = {
+    name: string;
+    age: number;
+};
+
+const person3: Person = {
+    name: 'John',
+    age: 30,
+};
+
+const person4: Person = {
+    name: 'John',
+    age: 30,
+};
+
+// String type alias and it can be used as type of anything afterwards
+type Username = string;
+
+type Student = {
+    name: Username;
+    age: number;
+};
+
+// Type alias for function
+type Add = (a: number, b: number) => number;
+
+const add: Add = (a, b) => a + b;
+
+console.log(add(1, 2));
