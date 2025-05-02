755e4beb17bd04ac43390bdda453d9405050add6
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-05-02 22:50:24 +0600

    generic type

--- dist/index.js
+++ dist/index.js
@@ -1,33 +1,9 @@
 "use strict";
-// ? types vs interfaces
-// By default, you should use interfaces where the properties of an object can be changed and you need specific features of interface like `extends`. Otherwise, use types.
-const user1 = {
-    name: 'John Doe',
-    age: 30,
-    email: 'lEY6n@example.com',
-    id: 1,
-};
-console.log('🚀 ~ user1 (created with interface):', user1);
-const admin1 = {
-    name: 'Jane Doe',
-    age: 25,
-    email: '3BhZ3@example.com',
-    id: 2,
-    role: 'admin',
-};
-console.log('🚀 ~ admin1 (created with interface):', admin1);
-const user2 = {
-    name: 'Jane',
-    age: 18,
-    email: 'you@domain.com',
-    id: 3,
-};
-console.log('🚀 ~ user2 (created with type):', user2);
-const admin2 = {
-    name: 'jane',
-    age: 20,
-    email: 'a@l.com',
-    id: 2,
-    role: 'admin',
-};
-console.log('🚀 ~ admin2 (created with type):', admin2);
+const stringList = ['a', 'b', 'c', 'd'];
+const numberList = [1, 2, 3, 4];
+const objectList = [
+    { id: 1, name: 'jhon' },
+];
+console.log('🚀 ~ objectList:', objectList);
+const rifat = ['rifat', 'male', true];
+console.log('🚀 ~ rifat:', rifat);
--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,17 @@
-// ? types vs interfaces
-// By default, you should use interfaces where the properties of an object can be changed and you need specific features of interface like `extends`. Otherwise, use types.
+// ! Generic types with type aliases
+// Generic type is a generalized type (or same quality) of different types which shared by all.
 
-// ! interface
-interface User {
-    name: string;
-    age: number;
-    email: string;
-    readonly id: number;
-}
+type Lists<T> = Array<T>; // A Generic Array
 
-interface Admin extends User {
-    role: 'admin';
-}
+const stringList: Lists<string> = ['a', 'b', 'c', 'd'];
+const numberList: Lists<number> = [1, 2, 3, 4];
+const objectList: Lists<{ readonly id: number; name: string }> = [
+    { id: 1, name: 'jhon' },
+];
 
-const user1: User = {
-    name: 'John Doe',
-    age: 30,
-    email: 'lEY6n@example.com',
-    id: 1,
-};
-console.log('🚀 ~ user1 (created with interface):', user1);
+console.log('🚀 ~ objectList:', objectList);
 
-const admin1: Admin = {
-    name: 'Jane Doe',
-    age: 25,
-    email: '3BhZ3@example.com',
-    id: 2,
-    role: 'admin',
-};
-console.log('🚀 ~ admin1 (created with interface):', admin1);
+type GenericTuple<X, Y, Z> = [X, Y, Z]; // A Generic Tuple
 
-// ! type
-type UserType = {
-    name: string;
-    age: number;
-    email: string;
-    readonly id: number;
-};
-
-type AdminType = UserType & {
-    role: 'admin';
-};
-
-const user2: UserType = {
-    name: 'Jane',
-    age: 18,
-    email: 'you@domain.com',
-    id: 3,
-};
-console.log('🚀 ~ user2 (created with type):', user2);
-
-const admin2: AdminType = {
-    name: 'jane',
-    age: 20,
-    email: 'a@l.com',
-    id: 2,
-    role: 'admin',
-};
-console.log('🚀 ~ admin2 (created with type):', admin2);
-
-// Generally types and interface are same unless you use methods offered by them. Like, when you are using extends in interface, it will be faster than & method of type
+const rifat: GenericTuple<string, string, boolean> = ['rifat', 'male', true];
+console.log('🚀 ~ rifat:', rifat);
