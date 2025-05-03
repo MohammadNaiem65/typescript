c48e64e6256da16aa135198f2774ff08d6c840c0
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-05-03 12:30:06 +0600

    keyof operator

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,16 @@
 "use strict";
-// ! Generic function with constrains
-// A generic function with constrains is a function that has a type parameter that has a constraint on it. The constraint is a type that must be satisfied by the type parameter.
-function user(params) {
-    // In this example, the name and email properties are required in the type parameter T which constrains the type of params.
-    return Object.assign(Object.assign({}, params), { id: Math.floor(Math.random() * 1000), name: params.name, email: params.email });
+// keyof operator
+function getProperty(obj, key) {
+    return obj[key];
 }
-const user1 = user({ name: 'user 1', email: 'user@user.com' });
-console.log('🚀 ~ user1:', user1);
-const user2 = user({ name: 'user 2', email: 'user@user.com', adult: true });
-console.log('🚀 ~ user2:', user2);
+const x = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+};
+console.log('🚀 ~ x:', x);
+console.log('🚀 ~ getProperty(x, "a"):', getProperty(x, 'a'));
+console.log('🚀 ~ getProperty(x, "b"):', getProperty(x, 'b'));
+console.log('🚀 ~ getProperty(x, "c"):', getProperty(x, 'c'));
+console.log('🚀 ~ getProperty(x, "d"):', getProperty(x, 'd'));
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-// ! Generic function with constrains
-// A generic function with constrains is a function that has a type parameter that has a constraint on it. The constraint is a type that must be satisfied by the type parameter.
-function user<T extends { name: string; email: string }>(params: T) {
-    // In this example, the name and email properties are required in the type parameter T which constrains the type of params.
-    return {
-        ...params,
-        id: Math.floor(Math.random() * 1000),
-        name: params.name,
-        email: params.email,
-    };
+// keyof operator : create a union (|) of all the keys of an object
+
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+    return obj[key];
 }
 
-const user1 = user({ name: 'user 1', email: 'user@user.com' });
-console.log('🚀 ~ user1:', user1);
+const x = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+};
 
-const user2 = user({ name: 'user 2', email: 'user@user.com', adult: true });
-console.log('🚀 ~ user2:', user2);
+console.log('🚀 ~ x:', x);
+console.log('🚀 ~ getProperty(x, "a"):', getProperty(x, 'a'));
+console.log('🚀 ~ getProperty(x, "b"):', getProperty(x, 'b'));
+console.log('🚀 ~ getProperty(x, "c"):', getProperty(x, 'c'));
+console.log('🚀 ~ getProperty(x, "d"):', getProperty(x, 'd'));
