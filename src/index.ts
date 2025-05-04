4e35bd45314d2fa248cdf495bd432926727ef16e
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-05-04 11:57:08 +0600

    completed task 3

--- src/index.ts
+++ src/index.ts
@@ -40,3 +40,49 @@
   const user = createUser("John", 21, "admin");
   // console.log("🚀 ~ user:", user);
 }
+
+// Task 3: Define a structured Person object using Type Aliases.
+// Instructions:
+// Define a Person type alias with properties for Name, Address, Hair and Eye Color, Income and Expense, Hobbies, Family Members, Job, Skills, Marital Status, and Friends.
+{
+  type Person = {
+    name: string;
+    address: string;
+    hairColor: "black" | "brown";
+    eyeColor: "black" | "brown";
+    income?: number;
+    expense: number;
+    hobbies: string[];
+    familyMembers?: Person[];
+    job?: string;
+    skills?: string[];
+    maritalStatus: boolean;
+    friends?: Person[];
+  };
+
+  const person1: Person = {
+    name: "naiem",
+    address: "dhk",
+    hairColor: "black",
+    eyeColor: "brown",
+    income: 10000,
+    expense: 22000,
+    hobbies: ["gardening", "coding"],
+    job: "frontend dev",
+    maritalStatus: true,
+  };
+  // console.log("🚀 ~ person1:", person1);
+
+  const person2: Person = {
+    name: "fardin",
+    address: "dhk",
+    hairColor: "black",
+    eyeColor: "black",
+    income: 0,
+    expense: 5000,
+    hobbies: ["sleeping"],
+    maritalStatus: false,
+    familyMembers: [person1],
+  };
+  // console.log("🚀 ~ person2:", person2);
+}
